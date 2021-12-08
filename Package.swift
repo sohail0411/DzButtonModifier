c2766532d78bf194c63940425e60b0adc0f67c37
sohail.shaik <sohail.shaik@diatoz.com>
2021-12-08 15:21:55 +0530

    added  Dz ButtonModifier

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import Foundation
 
 let package = Package(
     name: "DzButtonModifier",
@@ -13,16 +14,13 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        //.package(url: "https://git.diatoz.com/diatoz-academy/swiftui-bootstrap-components/dz-button-modifier.git" , .exact("1.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DzButtonModifier",
-            dependencies: []),
-        .testTarget(
-            name: "DzButtonModifierTests",
-            dependencies: ["DzButtonModifier"]),
+            dependencies: [])
     ]
 )
--- Sources/DzButtonModifier/DzButtonModifier.swift
+++ Sources/DzButtonModifier/DzButtonModifier.swift
@@ -1,6 +1,76 @@
-public struct DzButtonModifier {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13.0, *)
+public struct DzButtonModifier : ViewModifier {
+  var font:Font
+  var backgroundColor:Color
+  var width:CGFloat
+  var height:CGFloat
+  var alignment:Alignment
+  var cornerRadius:CGFloat
+  var foregroundColor:Color
+  var overlaylineWidth:CGFloat
+  var strokeColor:Color
+    
+  public  init(font: Font, backgroundColor: Color, width: CGFloat,height:CGFloat,alignment:Alignment,cornerRadius:CGFloat,foregroundColor:Color,overlaylineWidth:CGFloat,strokeColor:Color) {
+           self.font = font
+           self.backgroundColor = backgroundColor
+           self.width = width
+           self.height = height
+           self.alignment = alignment
+           self.cornerRadius = cornerRadius
+           self.foregroundColor = foregroundColor
+           self.overlaylineWidth = overlaylineWidth
+           self.strokeColor = strokeColor
+
+       }
+ 
+public func body(content: Content) -> some View {
+    content
+      .font(font)
+      .frame(width: width, height: height, alignment: alignment)
+      .foregroundColor(foregroundColor)
+      .background(backgroundColor)
+      .cornerRadius(cornerRadius)
+      .overlay(RoundedRectangle(cornerRadius: cornerRadius).stroke(strokeColor, lineWidth: overlaylineWidth))
+
+  }
+}
+
+@available(iOS 13.0, *)
+public  extension View {
+@available(iOS 13.0, *)
+    func DzButtonModifiers(font: Font,backgroundColor:Color,width:CGFloat,height:CGFloat,alignment:Alignment,cornerRadius:CGFloat,foregroundColor:Color,overlaylineWidth:CGFloat,strokeColor:Color) -> some View {
+        
+        modifier(DzButtonModifier(font: font, backgroundColor: backgroundColor, width: width, height: height, alignment: alignment, cornerRadius: cornerRadius,foregroundColor: foregroundColor,overlaylineWidth: overlaylineWidth,strokeColor: strokeColor))
+    }
+}
+
+
+@available(iOS 13.0, *)
+public struct DzButton: View {
+    var buttonAction: () -> Void
+    var buttonTitle : String
+    var font:Font
+    var backgroundColor:Color = .blue
+    var width:CGFloat
+    var height:CGFloat
+    var alignment:Alignment = .center
+    var cornerRadius:CGFloat = 2
+    var foregroundColor:Color = .white
+    var overlaylineWidth:CGFloat = 1
+    var strokeColor:Color = .white
+    
+    public  var body: some View {
+        Button(action: buttonAction) {
+            VStack{
+                Text(buttonTitle)
+                .DzButtonModifiers(font:font, backgroundColor:backgroundColor,width:width,height:height,alignment:alignment,cornerRadius:cornerRadius,foregroundColor:foregroundColor,overlaylineWidth:overlaylineWidth,strokeColor:strokeColor)
+                }
+        }
+        
     }
 }
